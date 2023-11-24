70b7032b699e75a6d38dab8c68b28d506e9f83e9
ilyes-chabab <ilyes.chabab@laplateforme.io>
2023-11-24 14:32:23 +0100

    new

--- jour05/job04/job04.py
+++ jour05/job04/job04.py
@@ -1,6 +1,21 @@
-def function():
-    fruits=["pomme","cerise","orange","melon"]
-    fruits[2]="mangue"
-    return fruits
-var=function()
-print(var)
+n=1
+n= n + int(input("quel longeur voulez-vous ? : "))
+ligne="+"+n*"-"+"+"
+print(ligne)
+for _ in range(n):
+    debut= n
+    fin=n-n
+    for _ in range(debut):
+        if debut==0:
+            break
+        else:           
+            debut= debut -1
+            print("|"+debut*"#"+" "+fin*"#" +"|")
+            fin = fin +1 
+        # while debut >0:
+        #     debut= debut -1
+        #     print("|"+debut*"#"+" "+fin*"#" +"|")
+        #     fin = fin +1 
+        #     break
+print(ligne)          
+   
